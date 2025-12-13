77d7ba386db3cbc88379855abb8a1ee5121dbfc9
Parsa Nasirimehr <parsa.nasirimehr@umain.com>
2025-12-13 14:01:18 +0100

    chore: rename repo and clean up

--- __tests__/repos/TicketRepo/TicketRepo.spec.ts
+++ __tests__/repos/TicketRepo/TicketRepo.spec.ts
@@ -5,16 +5,16 @@ import { TicketRepoImpl } from '../../../src/repos/TicketRepo/TicketRepo.ts';
 describe('TicketRepo tests', () => {
   const formatter = mock<CurrencyFormatter>();
 
-  function createOrderRepo() {
+  function createTicketRepo() {
     return new TicketRepoImpl(formatter);
   }
 
   test('See if testing works', () => {
     // Given
-    const orderRepo = createOrderRepo();
+    const ticketRepo = createTicketRepo();
     formatter.format.mockImplementation(input => `${input}`);
     // When
-    const result = orderRepo.latestItem();
+    const result = ticketRepo.latestItem();
     // Then
     expect(result).toBe('1234');
     expect(formatter.format).toHaveBeenCalledTimes(1);
